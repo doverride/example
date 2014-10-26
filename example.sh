6ae9e4d838f714bb33bfd95f1ab600aa6d81ba12
Dominick Grift <dac.override@gmail.com>
2014-10-26 15:58:21 +0100

    First commit

---
+++ example.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+test -d policy || /bin/mkdir policy
+
+EXCLUDE="tests"
+
+gen_listing ()
+
+{
+    local MODULES=$(/bin/find . -type f \( -iname "*.cil" \) | /bin/cut -d/ -f2-)
+
+    for i in $EXCLUDE ;
+        do
+            MODULES=$(printf "%s\n" "$MODULES" | /bin/grep -v $i)
+        done
+
+    printf "%s\n" "$MODULES" | /bin/sort > LISTING
+}
+
+/usr/bin/secilc -v \
+    $(test -f LISTING || gen_listing && /bin/cat LISTING) \
+    $* \
+    -o policy/policy.29 -f contexts/files/file_contexts
